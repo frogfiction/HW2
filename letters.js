ef2eeb9f005bfd97658776cc4fa5f5ef4ba37502
frogfiction <60366027+frogfiction@users.noreply.github.com>
2020-02-07 22:17:48 -0800

    Update letters.js

--- letters.js
+++ letters.js
@@ -1,1 +1,208 @@
+function setup() {
+  createCanvas(350, 650);
+}
 
+function printA() {
+  line(50, 30, 30, 100);
+  line(50, 30, 75, 100);
+  line(40, 70, 60, 70);
+}
+
+function printB() {
+  line(100, 30, 100, 100);
+  line(130, 30, 130, 100);
+  line(100, 30, 130, 30);
+  line(100, 70, 130, 70);
+  line(100, 100, 130, 100);
+}
+
+function printC() {
+  line(160, 30, 160, 100);
+  line(160, 30, 190, 30);
+  line(160, 100, 190, 100);
+}
+
+function printD() {
+  line(220, 30, 220, 100);
+  line(220, 30, 250, 75);
+  line(250, 75, 220, 100);
+}
+
+function printE() {
+  line(280, 30, 280, 100);
+  line(280, 30, 310, 30);
+  line(280, 70, 310, 70);
+  line(280, 100, 310, 100);
+}
+
+function printF() {
+  line(30, 130, 30, 200);
+  line(30, 130, 60, 130);
+  line(30, 170, 60, 170);
+}
+
+function printG() {
+  line(100, 130, 100, 200);
+  line(100, 130, 130, 130);
+  line(100, 200, 130, 200);
+  line(130, 200, 130, 170);
+}
+
+function printH() {
+  line(160, 130, 160, 200);
+  line(195, 130, 195, 200);
+  line(160, 170, 195, 170);
+}
+
+function printI() {
+  line(235, 130, 235, 200);
+  line(220, 130, 250, 130);
+  line(220, 200, 250, 200);
+}
+
+function printJ() {
+  line(310, 130, 310, 200);
+  line(280, 200, 310, 200);
+  line(280, 200, 280, 170);
+}
+
+function printK() {
+  line(30, 230, 30, 300);
+  line(30, 275, 60, 230);
+  line(30, 275, 60, 300);
+}
+
+function printL() {
+  line(100, 230, 100, 300);
+  line(100, 300, 130, 300);
+
+}
+
+function printM() {
+  line(160, 230, 160, 300);
+  line(195, 230, 195, 300);
+  line(160, 230, 177, 275);
+  line(195, 230, 177, 275);
+
+}
+
+function printN() {
+  line(220, 230, 220, 300);
+  line(250, 230, 250, 300);
+  line(220, 230, 250, 300);
+}
+
+
+function printO() {
+  line(280, 230, 280, 300);
+  line(280, 230, 310, 230);
+  line(280, 300, 310, 300);
+  line(310, 230, 310, 300);
+}
+
+function printP() {
+  line(30, 330, 30, 400);
+  line(30, 330, 60, 330);
+  line(60, 330, 60, 375);
+  line(60, 375, 30, 375);
+}
+
+function printQ() {
+  line(100, 330, 100, 400);
+  line(100, 400, 130, 400);
+  line(130, 400, 130, 330);
+  line(130, 330, 100, 330);
+  line(120, 390, 130, 410);
+
+}
+
+function printR() {
+  line(160, 330, 160, 400);
+  line(160, 330, 195, 330);
+  line(160, 370, 195, 370);
+  line(195, 330, 195, 370);
+  line(160, 370, 195, 400);
+}
+
+function printS() {
+  line(220, 330, 220, 370);
+  line(220, 330, 250, 330);
+  line(220, 370, 250, 370);
+  line(250, 370, 250, 400);
+  line(250, 400, 220, 400);
+}
+
+function printT() {
+  line(280, 330, 310, 330);
+  line(295, 330, 295, 400);
+}
+
+
+function printU() {
+  line(30, 430, 30, 500);
+  line(30, 500, 60, 500);
+  line(60, 500, 60, 430);
+}
+
+function printV() {
+  line(100, 430, 115, 500);
+  line(115, 500, 130, 430);
+}
+
+function printW() {
+  line(160, 430, 160, 500);
+  line(160, 500, 175, 465);
+  line(175, 465, 195, 500);
+  line(195, 500, 195, 430);
+}
+
+function printX() {
+  line(220, 430, 250, 500);
+  line(220, 500, 250, 430);
+}
+
+
+function printY() {
+  line(280, 430, 295, 465);
+  line(295, 465, 310, 430);
+  line(295, 465, 295, 500);
+}
+
+function printZ() {
+  line(30, 530, 60, 530);
+  line(60, 530, 30, 600);
+  line(30, 600, 60, 600);
+}
+
+
+
+function draw() {
+  background(255, 204, 0);
+  strokeWeight(10);
+  printA();
+  printB();
+  printC();
+  printD();
+  printE();
+  printF();
+  printG();
+  printH();
+  printI();
+  printJ();
+  printK();
+  printL();
+  printM();
+  printN();
+  printO();
+  printP();
+  printQ();
+  printR();
+  printS();
+  printT();
+  printU();
+  printV();
+  printW();
+  printX();
+  printY();
+  printZ();
+}
